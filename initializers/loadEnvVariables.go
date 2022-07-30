dd67eba38785a0701a42115291f8d777f75c0d3d
Mani Movassagh <mani.mgh@gmail.com>
2022-07-30 13:36:38 +0200

    .env done

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -9,7 +9,7 @@ import (
 func LoadEnvVariables() {
 	err := godotenv.Load("../.env")
 	if err != nil {
-		log.Fatal("Error loading .env file in This case")
+		log.Fatal("Error loading in .env file")
 	}
 
 }
