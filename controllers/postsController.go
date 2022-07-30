d2a37d16918851e19109f2f4c5ddaf797dad6ba7
Mani Movassagh <mani.mgh@gmail.com>
2022-07-30 14:02:25 +0200

    post request working

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -1,9 +1,22 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"app/initializers"
+	"app/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 func PostsCreate(c *gin.Context) {
+
+	post := models.Post{Title: "Mani", Body: "Test The Body"}
+
+	result := initializers.DB.Create(&post) // pass pointer of data to Create
+	if result.Error != nil {
+		c.JSON(400, gin.H{"error": result.Error})
+		return
+	}
 	c.JSON(200, gin.H{
-		"message": "pong",
+		"message": post,	
 	})
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"app/controllers"
 	"app/initializers"
 
 	"github.com/gin-gonic/gin"
@@ -16,10 +17,7 @@ func main() {
 	r := gin.Default()
 	r.SetTrustedProxies([]string{"127.0.0.1"})
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/posts", controllers.PostsCreate)
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
