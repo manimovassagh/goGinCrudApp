a05a7ef19aba62c5dd5394462eaa7dcce2a449a3
Mani Movassagh <mani.mgh@gmail.com>
2022-07-30 12:44:54 +0200

    define more

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,1 +1,15 @@
-package migrate
+package main
+
+import (
+	"app/initializers"
+	"app/models"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	initializers.DB.AutoMigrate(&models.Post{})
+}
